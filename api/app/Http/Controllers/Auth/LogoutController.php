3f644bfa9ba263f6734215247d453ac442b15597
amersalar <amersalar108@gmail.com>
2026-08-18 13:40:10 +0300

    feat: logout user

--- api/app/Http/Controllers/Auth/LogoutController.php
+++ api/app/Http/Controllers/Auth/LogoutController.php
@@ -4,14 +4,16 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class LogoutController extends Controller
 {
-    /**
-     * Handle the incoming request.
-     */
-    public function __invoke(Request $request)
+    public function __invoke()
     {
-        //
+        Auth::logout();
+
+        return response()->json([
+            'message' => 'logged-out successfully and token invalidated.'
+        ], 200);
     }
 }
--- api/routes/api.php
+++ api/routes/api.php
@@ -1,8 +1,13 @@
 <?php
 
 use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\LogoutController;
 use App\Http\Controllers\Auth\RegisterController;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/register', RegisterController::class)->name('register');
 Route::post('/login', LoginController::class)->name('login');
+
+Route::middleware('auth:api')->group(function () {
+    Route::post('/logout', LogoutController::class)->name('logout');
+});
