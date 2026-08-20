4c6aacf4b68d6c03737bc69ae909db402eb8de2b
amersalar <amersalar108@gmail.com>
2026-08-20 17:10:38 +0300

    feat: attempt to implement push item to cart

--- api/app/Http/Controllers/API/CartController.php
+++ api/app/Http/Controllers/API/CartController.php
@@ -9,6 +9,7 @@ use App\Models\Product;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 class CartController extends Controller
 {
@@ -20,7 +21,26 @@ class CartController extends Controller
     }
     public function push(Request $request, Product $product)
     {
-        $request->user()->cart->items()->attach($product);
+        $request->validate(['quantity' => ['nullable', 'integer', 'min:1', 'max:50']]);
+        $quantity = $request->input('quantity') ?? 1;
+        $cartItemQuantity = 0;
+
+        if ($request->user()->cart->items()->where('product_id', $product)->exists()) {
+
+            $cartItemQuantity = DB::table('cart_items')->whereRaw(
+                'cart_id = ? AND product_id = ?',
+                [
+                    $request->user()->cart->id,
+                    $product->id
+                ]
+            )->first()->quantity;
+        }
+
+
+        $request->user()->cart->items()->syncWithoutDetaching([
+            $product->id => ['quantity' => $quantity + $cartItemQuantity]
+        ]);
+
 
         $cart = $request->user()->cart->load('items');
         return new CartResource($cart);
