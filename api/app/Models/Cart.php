6b76c6e4909460596026d94b49ca778f40f06ae2
amersalar <amersalar108@gmail.com>
2026-08-21 16:37:21 +0300

    feat: total payment amount of cart calculated on accessing (accessors)

--- api/app/Models/Cart.php
+++ api/app/Models/Cart.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
@@ -17,5 +18,13 @@ class Cart extends Model
         return $this->belongsToMany(Product::class, 'cart_items')
             ->withPivot('quantity')
             ->withTimestamps();
+    }
+    public function total(): Attribute
+    {
+        return Attribute::make(
+            get: fn() => $this->items->sum(
+                fn($product) => $product->price * $product->pivot->quantity
+            )
+        );
     }
 }
