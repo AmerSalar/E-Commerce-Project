1b7f8e1083ebcf3569cca72ee752a7ec3b682acb
amersalar <amersalar108@gmail.com>
2026-08-21 18:54:45 +0300

    feat: order now method in progress

--- api/app/Http/Controllers/API/OrderController.php
+++ api/app/Http/Controllers/API/OrderController.php
@@ -3,17 +3,82 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Address\AddressSnapshotRequest;
+use App\Models\Order;
+use App\Models\Product;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class OrderController extends Controller
 {
-    public function orderNow(Request $request)
+    public function orderNow(AddressSnapshotRequest $request)
     {
+        $validatedAddress = $request->validated();
+
+        // we first make sure cart has items
         $cart = $request->user()->cart?->load('items');
         if (!$cart || $cart->items->isEmpty()) {
             return response()->json([
                 'message' => 'There is nothing to order, cart is empty!'
             ], 422);
         }
+
+        // we add a transaction to safely do the actions
+        $order = DB::transaction(function () use ($request, $cart, $validatedAddress) {
+            // we lock products to avoid stock updates
+            $productIds = $cart->items->pluck('id');
+            $products = Product::whereIn('id', $productIds)
+                ->lockForUpdate()
+                ->get()
+                ->keyBy('id');
+
+
+            // validate that item is 'still' available in stock
+            foreach ($cart->items as $item) {
+                $cartQuantity = $item->pivot->quantity;
+                $product = $products->get($item->id);
+
+                if ($product->quantity < $cartQuantity) {
+                    return response()->json([
+                        'message' => "Sorry, {$product->name} is out of stock!",
+                        'available_stock' => $product->quantity,
+                        'in_cart' => $cartQuantity
+                    ], 422);
+                }
+            }
+
+            // create the order for the user, and calculate the total
+            $order = Order::create([
+                'user_id' => $request->user()->id,
+                'address_snapshot' => $validatedAddress,
+                'total' => $cart->total
+            ]);
+
+            // attach the order items from the cart items
+            foreach ($cart->items as $item) {
+                $order->items()->attach([
+                    'product_id' => $item->id,
+                    'item_name' => $item->name,
+                    'item_price' => $item->price,
+                    'quantity' => $item->pivot->quantity,
+                    'created_at' => now(),
+                    'updated_at' => now()
+                ]);
+
+                // take the quantity from the stock
+                $products->get($item->id)
+                    ->decrement('quantity', $item->pivot->quantity);
+            }
+
+            // empty the cart
+            $cart->items()->detach();
+
+            return $order;
+        });
+
+        return response()->json([
+            'message' => 'ordered successfully. order is now pending.',
+            'order' => $order
+        ]);
     }
 }
