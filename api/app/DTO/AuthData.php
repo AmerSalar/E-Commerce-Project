a9aeb8879b6ee6936621389055e693ef9220b3df
amersalar <amersalar108@gmail.com>
2026-08-25 17:05:09 +0300

    feat: laravel telescope

--- api/app/DTO/AuthData.php
+++ api/app/DTO/AuthData.php
@@ -6,6 +6,8 @@ namespace App\DTO;
 readonly class AuthData
 {
     public function __construct(
+        // this is an example of IoC and service container.
+        // dependencies will automatically be injected
         public mixed $user,
         public string $token,
         public string $message = "Success"
