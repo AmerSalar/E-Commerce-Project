508ccfc070ba9eee32a641aa15fa8d6e0750df7e
amersalar <amersalar108@gmail.com>
2026-09-02 10:52:56 +0300

    feat: default game cover placeholder

--- api/database/migrations/2026_08_18_182644_create_products_table.php
+++ api/database/migrations/2026_08_18_182644_create_products_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -17,7 +16,7 @@ return new class extends Migration
             $table->text('description')->nullable();
             $table->decimal('price', 8, 2);
             $table->unsignedInteger('quantity');
-            $table->string('picture_url')->nullable();
+            $table->string('picture_url')->default("/assets/cover.webp");
             $table->timestamps();
         });
     }
